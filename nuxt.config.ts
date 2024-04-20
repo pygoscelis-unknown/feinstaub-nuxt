84a547c1c01c752d350a8597c3888556b4f17874
MogliCodes <moglicodes@gmail.com>
2024-04-20 14:17:31 +0200

    chore(deps): update all dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL || 'https://feinstaub-django.onrender.com'
+    }
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   ui: {
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,30 @@
             <div class="w-1/2">
                 <h1 class="text-5xl mb-6">feinstaub.app headline für hero section</h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, aperiam? Quia ipsam est molestiae eaque aperiam, dolorem sed nulla magnam quae, assumenda aut vero! Incidunt voluptas soluta optio dignissimos ratione hic rerum voluptates dicta dolor! Eaque soluta quibusdam libero fuga autem possimus, reiciendis aliquam nostrum totam ea, doloremque perspiciatis quas!</p>  
+              <div v-for='item in data'>
+                <pre>
+                  {{ item }}
+                </pre>
+              </div>
+              <pre>
+                  {{ data }}
+                </pre>
+              <pre>
+                  {{ pending }}
+                </pre>
+              <pre>
+                  {{ error }}
+                </pre>
             </div>
         </BaseContainer>
     </section>
 </template>
+
+<script setup lang='ts'>
+
+const config = useRuntimeConfig()
+const { data, pending, error } = useFetch(`${config.public.apiBaseUrl}/test`)
+
+console.log('testData', data)
+
+</script>
